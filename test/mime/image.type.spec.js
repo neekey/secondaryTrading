0428613c47c4e1a33b2ca7b5761c1d665084b6e1
neekey <ni184775761@gmail.com>
2012-01-26 16:37:48 +0800

    修改图片格式判断模块的单元测试

--- test/mime/image.type.spec.js
+++ test/mime/image.type.spec.js
@@ -4,67 +4,195 @@
  */
 
 var Mime = require( '../../mime/' );
-var JPEG_PATH = 'images/jpeg.jpg';
-var PNG_PATH = 'images/png.png';
-var GIF_PATH = 'images/gif.gif';
-var BMP_16_PATH = 'images/bmp.16.bmp';
-var BMP_32_PATH = 'images/bmp.32.bmp';
-var BMP_24_PATH = 'images/bmp.24.bmp';
-var UNDEFINED_PATH = 'images/undefined.text';
+var JPEG_PATH = __dirname + '/images/jpeg.jpg';
+var PNG_PATH = __dirname + '/images/png.png';
+var GIF_PATH = __dirname + '/images/gif.gif';
+var BMP_16_PATH = __dirname + '/images/bmp.16.bmp';
+var BMP_32_PATH = __dirname + '/images/bmp.32.bmp';
+var BMP_24_PATH = __dirname + '/images/bmp.24.bmp';
+var UNDEFINED_PATH = __dirname + '/images/undefined.txt';
 
 describe( '根据文件头判断文件类型', function(){
 
     it( 'JPEG测试', function(){
 
-        Mime.parse( JPEG_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'jpeg' );
+        runs(function(){
+            Mime.parse( JPEG_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'jpeg' );
         });
     });
 
     it( 'PNG测试', function(){
 
-        Mime.parse( PNG_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'png' );
+        runs(function(){
+            Mime.parse( PNG_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'png' );
         });
     });
 
     it( 'GIF测试', function(){
 
-        Mime.parse( GIF_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'gif' );
+        runs(function(){
+            Mime.parse( GIF_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'gif' );
         });
     });
 
     it( 'BMP16测试', function(){
 
-        Mime.parse( BMP_16_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'bmp' );
+        runs(function(){
+            Mime.parse( BMP_16_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'bmp' );
         });
     });
 
     it( 'BMP24测试', function(){
 
-        Mime.parse( BMP_24_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'bmp' );
+        runs(function(){
+            Mime.parse( BMP_24_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'bmp' );
         });
     });
 
     it( 'BMP32测试', function(){
 
-        Mime.parse( BMP_32_PATH, function( err, type ){
+        var t;
+        var e;
+        var finished = false;
 
-            expect( typeof err ).toEqual( 'undefined' );
-            expect( typeof type ).toEqual( 'bmp' );
+        runs(function(){
+            Mime.parse( BMP_32_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( t ).toEqual( 'bmp' );
+        });
+    });
+
+    it( '未定义格式测试', function(){
+
+        var t;
+        var e;
+        var finished = false;
+
+        runs(function(){
+            Mime.parse( UNDEFINED_PATH, function( err, type ){
+                e = err;
+                t = type;
+                finished = true;
+            });
+        });
+
+        waitsFor( function(){
+
+            return finished;
+        });
+
+        runs( function(){
+
+            expect( typeof e ).toEqual( 'undefined' );
+            expect( typeof t ).toEqual( 'undefined' );
         });
     });
 });
